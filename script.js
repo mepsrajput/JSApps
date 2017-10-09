452b31b6b23bf55ef46f8533dbc3fe7880eff12e
Pradeep Singh <viveksingh12126@gmail.com>
2017-10-09 20:42:55 +0530

    files added

--- script.js
+++ script.js
@@ -17,6 +17,39 @@ function calculate() {
 		+ "Perimeter: " + p; 
 	}
 }
-			
+		
+
+$(document).ready(function () {
+
+	$('.goTop').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 1000);
+			return false;
+		});
+
+	function scrollNav() {
+		$('.nav a').click(function(){  
+			//Animate
+			$('html, body').stop().animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+			}, 800);
+		return false;
+	});
+	$('.scrollTop a').scrollTop();
+	}
+	scrollNav();
+
+    $(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");               
+    var _opened = $navbar.hasClass("in");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+        $navbar.collapse('hide');
+    }
+	});
+	           
+
+});		
 			
 			
