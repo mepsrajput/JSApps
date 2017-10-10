324cd398e77c8ba2e1f5f7646cde795e21be8d73
Pradeep Singh <viveksingh12126@gmail.com>
2017-10-10 10:51:17 +0530

    App1 redesigned, bug fixed

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function calculate() {
-	var a = document.getElementById('side1').value
-	, b = document.getElementById('side2').value;
 
-	var p = (a * 2 ) + (b * 2);
-
-	var area = (a * b)
-	
-	if(isNaN(a) || isNaN(b)){
-		document.getElementById('data').innerHTML = "Please  enter numbers only";
-		return;
-	} else if ( a <= 0 || b <=0 ){
-		document.getElementById('data').innerHTML = "Negative numbers and Zero don't make sense";
-		return;
-    } else {
-		document.getElementById('data').innerHTML = "Area: " + area + " " 
-		+ "Perimeter: " + p; 
-	}
-}
-		
-
+// jquery starts here
 $(document).ready(function () {
 
 	$('.goTop').click(function () {
@@ -48,8 +29,67 @@ $(document).ready(function () {
         $navbar.collapse('hide');
     }
 	});
-	           
+});// jquery ends here
 
-});		
 			
-			
+function calculateQuad() {
+	var l = document.getElementById('length').value
+	, b = document.getElementById('breadth').value;
+
+	var perimeter = ((l * 2 ) + (b * 2)).toFixed(2);
+
+	var area = (l * b).toFixed(2);
+	
+	if(isNaN(l) || isNaN(b)){
+		document.getElementById('data').innerHTML = "Please  enter numbers only";
+		return;
+	} else if ( l <= 0 || b <=0 ){
+		document.getElementById('data').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data').innerHTML = "Area: " + area + " " 
+		+ "Perimeter: " + perimeter; 
+	}
+}
+
+
+
+function calculateTri() {
+	var s1 = document.getElementById('side1').value, 
+		s2 = document.getElementById('side2').value, 
+		s3 = document.getElementById('side3').value;
+
+	var peri = ((s1 * 1) + (s2 * 1) + (s3 * 1)).toFixed(2);
+	
+	var p  = peri / 2;
+
+	var area2 = (Math.sqrt(p * (p - s1) * (p - s2) * (p - s3))).toFixed(2);
+	
+	if(isNaN(s1) || isNaN(s2)|| isNaN(s3)){
+		document.getElementById('data2').innerHTML = "Please  enter numbers only";
+		return;
+	} else if ( s1 <= 0 || s2 <=0 || s3 <=0){
+		document.getElementById('data2').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data2').innerHTML = "Area: " + area2 + " " + "Perimeter: " + peri; 
+	}
+}
+
+function calculateCircle() {
+	var r = document.getElementById('radius').value;
+
+	var circlePeri = (2 * Math.PI * r).toFixed(2);
+	
+	var circlAarea = (Math.PI * r * r).toFixed(2);
+	
+	if(isNaN(r)){
+		document.getElementById('data3').innerHTML = "Please  enter numbers only";
+		return;
+	} else if (r <= 0){
+		document.getElementById('data3').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data3').innerHTML = "Area: " + circlAarea + " " + "Perimeter: " + circlePeri; 
+	}
+}
