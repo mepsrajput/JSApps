39689a9a8d38ea3d177f652fd9d40dfe41ef59be
Pradeep Singh <viveksingh12126@gmail.com>
2017-10-22 14:42:15 +0530

    App2 added

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ $(document).ready(function () {
 	});
 });// jquery ends here
 
-			
+
+//#1 Quadrilateral			
 function calculateQuad() {
 	var l = document.getElementById('length').value
 	, b = document.getElementById('breadth').value;
@@ -47,13 +48,12 @@ function calculateQuad() {
 		document.getElementById('data').innerHTML = "Negative numbers and Zero don't make sense";
 		return;
     } else {
-		document.getElementById('data').innerHTML = "Area: " + area + " " 
-		+ "Perimeter: " + perimeter; 
+		document.getElementById('data').innerHTML = "Area: " + area	+ "<br />Perimeter: " + perimeter; 
 	}
 }
 
 
-
+//#2 Triangle
 function calculateTri() {
 	var s1 = document.getElementById('side1').value, 
 		s2 = document.getElementById('side2').value, 
@@ -72,10 +72,11 @@ function calculateTri() {
 		document.getElementById('data2').innerHTML = "Negative numbers and Zero don't make sense";
 		return;
     } else {
-		document.getElementById('data2').innerHTML = "Area: " + area2 + " " + "Perimeter: " + peri; 
+		document.getElementById('data2').innerHTML = "Area: " + area2 + "<br />Perimeter: " + peri; 
 	}
 }
 
+//#3 Circle
 function calculateCircle() {
 	var r = document.getElementById('radius').value;
 
@@ -90,6 +91,96 @@ function calculateCircle() {
 		document.getElementById('data3').innerHTML = "Negative numbers and Zero don't make sense";
 		return;
     } else {
-		document.getElementById('data3').innerHTML = "Area: " + circlAarea + " " + "Perimeter: " + circlePeri; 
+		document.getElementById('data3').innerHTML = "Area: " + circlAarea + "<br />Perimeter: " + circlePeri; 
 	}
+}
+
+
+//#4 Sphere
+function calculateSphere() {
+	var sr = document.getElementById('sphereRadius').value;
+
+	var sphereArea = (4 * Math.PI * sr * sr).toFixed(2);
+	
+	var sphereVol = ((4 * Math.PI * sr * sr * sr) / 3).toFixed(2);
+	
+	if(isNaN(sr)){
+		document.getElementById('data4').innerHTML = "Please  enter numbers only";
+		return;
+	} else if ( sr <=0 ){
+		document.getElementById('data4').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data4').innerHTML = "Surface Area: " + sphereArea + "<br />Sphere Volume: " + sphereVol; 
+	}
+}
+
+
+
+
+//#5 Cone
+function calculateCone() {
+	var cr = document.getElementById('coneRadius').value;
+	var cl = document.getElementById('coneHeight').value;
+
+	
+	var coneArea = ((Math.PI * cr) * ( cr + cl )).toFixed(2);
+	
+	var coneVol = (( Math.PI * cr * cr * cl ) / 3).toFixed(2);
+	
+	if(isNaN(cr) || isNaN(cl)){
+		document.getElementById('data5').innerHTML = "Please  enter numbers only";
+		return;
+	} else if (cr <= 0 || cl <= 0){
+		document.getElementById('data5').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data5').innerHTML = "Surface Area: " + coneArea + "<br /> Volume: " + coneVol; 
+	}
+
+}
+
+
+//#6 Cuboid
+function calculateCuboid() {
+	var cl = document.getElementById('cuboidLength').value;
+	var cb = document.getElementById('cuboidBreadth').value;
+	var ch = document.getElementById('cuboidHeight').value;
+	
+	var cuboidArea = (2 * ( (cl * cb) + (cb * ch) + (ch * cl ) ) ).toFixed(2);
+	
+	var cuboidVol = ( cl * cb * ch ).toFixed(2);
+	
+	if(isNaN(cl) || isNaN(cb) || isNaN(ch)){
+		document.getElementById('data6').innerHTML = "Please  enter numbers only";
+		return;
+	} else if (cl <= 0 || cb <= 0 || ch <= 0){
+		document.getElementById('data6').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data6').innerHTML = "Surface Area: " + cuboidArea + "<br /> Volume: " + cuboidVol; 
+	}
+
+}
+
+//#7 Cylinder
+function calculateCylinder() {
+	var cyh = document.getElementById('cylHeight').value;
+	var cyr = document.getElementById('cylRadius').value;
+
+	
+	var cylArea = ( 2 * Math.PI * cyr * ( cyr + cyh ) ).toFixed(2);
+	
+	var cylVol = ( Math.PI * cyr * cyr * cyh ).toFixed(2);
+	
+	if(isNaN(cyr) || isNaN(cyh)){
+		document.getElementById('data7').innerHTML = "Please  enter numbers only";
+		return;
+	} else if (cyr <= 0 || cyh <= 0){
+		document.getElementById('data7').innerHTML = "Negative numbers and Zero don't make sense";
+		return;
+    } else {
+		document.getElementById('data7').innerHTML = "Surface Area: " + cylArea + "<br /> Volume: " + cylVol; 
+	}
+
 }
